0470ebd82ea64ee0a005625ac5a4eb717213ede9
Ahmed-rbr <ahmed.rabar.dev@gmail.com>
2025-08-04 22:59:39 +0100

    using factory to add fake data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first-name' => fake()->firstName(),
+            'last-name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
@@ -40,5 +41,6 @@ class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
-    }
+    } 
+    
 }
